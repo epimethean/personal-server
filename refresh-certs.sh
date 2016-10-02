e18f0f17a5f7f6958e4618074b8179a8d5a024c3
Ellis Rahhal <ellis@guestops.com>
2016-10-02 13:17:45 -0700

    updated letsencrypt rc version; updated refresh-certs.sh to actually work

--- refresh-certs.sh
+++ refresh-certs.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-PROJECT_ID="aerobic-furnace-111811"
-
 kubectl delete rc nginx-rc
 kubectl create -f nginx-letsencrypt-rc.yaml
-NAME=$(kubectl get pods | grep nginx-rc | awk '{ print $1 }')
+while ! kubectl get pods | grep nginx-rc | grep Running > /dev/null; do
+    echo "waiting for nginx-rc letsencrypt pod to go up..."
+    sleep 1
+done
+NAME=$(kubectl get pods | grep nginx-rc | grep Running | awk '{ print $1 }')
+kubectl delete secret onru.sh
 kubectl exec -it $NAME /bin/bash -- -c 'EMAIL=ellis.rahhal@gmail.com DOMAINS=onru.sh ./fetch_certs.sh'
 kubectl exec -it $NAME /bin/bash -- -c 'SECRET_NAME=onru.sh DOMAINS=onru.sh ./save_certs.sh'
+kubectl delete secret rahh.al
 kubectl exec -it $NAME /bin/bash -- -c 'EMAIL=ellis.rahhal@gmail.com DOMAINS=rahh.al ./fetch_certs.sh'
-kubectl exec -it $NAME /bin/bash -- -c 'SECRET_NAME=onru.sh DOMAINS=rahh.al ./save_certs.sh'
+kubectl exec -it $NAME /bin/bash -- -c 'SECRET_NAME=rahh.al DOMAINS=rahh.al ./save_certs.sh'
 kubectl delete rc nginx-rc
 kubectl create -f nginx-rc.yaml
